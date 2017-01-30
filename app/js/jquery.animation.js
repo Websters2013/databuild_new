a5690ba3c0641ca8a850db0967e7dcb86b99fe80
petruninevgeniy <petruninevgeniy@gmail.com>
2017-01-30 13:18:38 +0200

    start project.

---
+++ app/js/jquery.animation.js
@@ -0,0 +1,63 @@
+
+( function() {
+    "use strict";
+
+    $( function() {
+
+        $.each( $( '.slides' ), function() {
+            new Slides ( $( this ) );
+        } );
+
+    });
+
+    var Slides = function ( obj ) {
+
+        //private properties
+        var _self = this,
+            _obj = obj,
+            _window = $( window );
+
+        //private methods
+        var _onEvents = function () {
+                _window.on({
+                    scroll: function () {
+
+                        _checkScroll();
+
+                    }
+                });
+            },
+            _checkScroll = function(){
+
+                var windowH = _window.height();
+
+                _obj.each(function () {
+
+                    var curItem = $(this),
+                        topPos = _obj.offset().top;
+
+                    if( _window.scrollTop() > (topPos - windowH/1.3) && !curItem.hasClass( 'animation' ) ){
+
+                        curItem.addClass( 'animation' );
+
+                    }
+                })
+            },
+            _init = function () {
+                _obj[0].slides = _self;
+                _onEvents();
+                _checkScroll();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
+} )();
+
+
+
+
---
+++ app/js/jquery.main.js
@@ -0,0 +1,195 @@
+"use strict";
+( function() {
+
+    $( function() {
+
+        $.each( $( '.site__menu' ), function() {
+
+            new Menu ( $( this ) );
+
+        } );
+
+        $.each( $( '.language' ), function(){
+
+            new Language ( $(this) )
+
+        } );
+
+    });
+
+    var Language = function (obj) {
+
+        //private properties
+        var _obj = obj,
+            _languagesDropDown = $( '.language__dropdown' ),
+            _languagesItem = $( '.language__item' ),
+            _languagesDropDownSpeed = 200,
+            _mouseenterTimeout,
+            _mouseleaveTimeout;
+
+        //private methods
+        var _addEvents = function () {
+
+                _languagesItem.on({
+                    click: function ( e ) {
+
+                        if( $( this ).parent().hasClass( 'language__active' ) ) {
+
+                            e.preventDefault();
+
+                        }
+
+                        if( $( this ).parent().hasClass( 'language__dropdown' ) ) {
+
+                            $( '.language__active .language__item' ).appendTo( _languagesDropDown );
+
+                            $( this ).appendTo( '.language__active' );
+
+                            _obj.removeClass( 'active' );
+                            _languagesDropDown.stop( true, true ).slideUp( _languagesDropDownSpeed );
+
+                        }
+                    }
+                });
+
+                _obj.on({
+                    mouseenter: function () {
+                        clearTimeout( _mouseleaveTimeout );
+
+                        _mouseenterTimeout = setTimeout( function() {
+
+                            _obj.addClass( 'active' );
+                            _languagesDropDown.stop( true, true ).slideDown( _languagesDropDownSpeed );
+
+                        }, 200);
+                    }
+                });
+
+                _obj.on({
+                    mouseleave: function () {
+
+                        clearTimeout( _mouseenterTimeout );
+
+                        _mouseleaveTimeout = setTimeout( function() {
+
+                            if(_languagesDropDown.is( ':visible' ) ) {
+
+                                _obj.removeClass( 'active' );
+                                _languagesDropDown.stop( true, true ).slideUp( _languagesDropDownSpeed );
+                            }
+
+                        }, 200);
+
+                    }
+                });
+
+            },
+
+            _init = function () {
+                _addEvents();
+            };
+
+        //public properties
+
+        //public methods
+
+
+        _init();
+    };
+
+    var Menu = function( obj ) {
+
+        //private properties
+        var _self = this,
+            _menu = obj,
+            _body = $( 'body' ),
+            _window = $( window ),
+            _showBtn = $( '.site__menu-btn' );
+
+        //private methods
+        var _onEvents = function() {
+
+                _showBtn.on( {
+                    click: function() {
+
+                        _openMenu( $( this ) );
+
+                    }
+                } );
+
+                _window.on( {
+                    resize: function () {
+
+                        _resetStyle();
+
+                    },
+                    scroll: function () {
+
+                        _checkScroll();
+
+                    }
+                } );
+
+            },
+            _checkScroll = function () {
+
+                var windowScroll = _window.scrollTop(),
+                    heroHeight = $('.site__hero').innerHeight();
+
+                if ( windowScroll > heroHeight ) {
+
+                    if ( !_showBtn.hasClass( 'active' ) ) {
+
+                        _showBtn.addClass('active')
+
+                    }
+
+                } else {
+
+                    _showBtn.removeClass('active')
+
+                }
+
+            },
+            _openMenu = function( elem )  {
+
+                var curItem = elem;
+
+                if( curItem.hasClass( 'opened' ) ) {
+
+                    curItem.removeClass( 'opened' );
+                    _menu.removeClass( 'opened' );
+
+                    _body.css( {
+                        'overflow': 'visible'
+                    } );
+
+                } else {
+
+                    curItem.addClass( 'opened' );
+                    _menu.addClass( 'opened' );
+
+                    _body.css( {
+                        'overflow': 'hidden'
+                    } );
+                }
+
+            },
+            _resetStyle = function() {
+
+                _showBtn.removeClass( 'opened' );
+                _menu.removeAttr( 'style' );
+                _body.css( {
+                    'overflow': 'visible'
+                } );
+
+            },
+            _init = function() {
+                _menu[ 0 ].obj = _self;
+                _onEvents();
+            };
+
+        _init();
+    };
+
+} )();
